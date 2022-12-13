25796606579c544990a4e6678e00fa1648f5534c
zubayermunna <muna700064@gmail.com>
2022-12-13 16:07:30 +0600

    loader included

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -11,9 +11,9 @@ const Services = () => {
   }, []);
 
   return (
-    <div className=" w-full bgRoom">
-      <div className="w-full flex h-80 justify-center pt-12 lg:pt-20 md:pt-20 items-center ">
-        <h1 className="text-4xl  text-white font-semibold">
+    <div className=" w-full flex flex-col  relative bgRoom">
+      <div className="w-full flex h-80 justify-center  items-center ">
+        <h1 className="text-4xl  py-24 mt-12 lg:mt-12 md:mt-12  lg:py-24 md:py-24 lg:pt-36 md:pt-36 text-white font-semibold">
           {" "}
           <TypewriterComponent
             onInit={(typewriter) => {
@@ -30,16 +30,40 @@ const Services = () => {
           />
         </h1>
       </div>
-      <div className="w-full h-fit justify-center items-center gap-12 py-10 bg-gray-100 flex flex-wrap">
-        {items.map((item) => (
-          <ServiceItem
-            name={item.name}
-            img={item.img}
-            price={item.price}
-            detail={item.detail}
-          ></ServiceItem>
-        ))}
-      </div>
+      {items.length === 6 ? (
+        <div className="w-full h-fit justify-center items-center gap-12 py-10 bg-gray-100 flex flex-wrap">
+          {items.map((item) => (
+            <ServiceItem
+              name={item.name}
+              img={item.img}
+              price={item.price}
+              detail={item.detail}
+            ></ServiceItem>
+          ))}
+        </div>
+      ) : (
+        <div className="w-full  flex justify-center items-center bg-gray-100 h-96">
+          <div
+            className="radial-progress animate-spin"
+            style={{ "--value": 70 }}
+          ></div>
+          <div className="text-2xl">
+            <TypewriterComponent
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Loading...")
+                  .pauseFor(100)
+                  .deleteAll()
+                  .typeString("Loading...")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Loading... ")
+                  .start();
+              }}
+            />
+          </div>
+        </div>
+      )}
       <Footer></Footer>
     </div>
   );
